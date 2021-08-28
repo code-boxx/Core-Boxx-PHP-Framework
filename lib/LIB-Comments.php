727350d5770674c1ed7cb5d429a70c0d362ac1a6
Code Boxx <code-boxx@mail.com>
2021-08-28 09:57:46 +0800

    Comments Module Update
    Added htmlentities.

--- lib/LIB-Comments.php
+++ lib/LIB-Comments.php
@@ -3,24 +3,29 @@ class Comments extends Core {
   // (A) SAVE COMMENT
   function save ($uid, $id, $message, $cid=null) {
     // (A1) ADD COMMENT
+    // @TODO - CHECK IF REGISTERED USER?
+    // if (!isset($_SESSION['user'])) { return false; }
     if ($cid===null) {
       return $this->DB->insert("comments",
         ["user_id", "id", "message"],
-        [$uid, $id, $message]
+        [$uid, $id, htmlentities($message)]
       );
     }
+
     // (A2) UPDATE COMMENT
+    // @TODO - ONLY ALLOW USER TO UPDATE OWN POST (OR ADMIN)
     else {
       return $this->DB->update("comments",
         ["user_id", "id", "message"],
         "`comment_id`=?",
-        [$uid, $id, $message, $cid]
+        [$uid, $id, htmlentities($message), $cid]
       );
     }
   }
 
   // (B) DELETE COMMENT
   function del ($cid) {
+    // @TODO - CHECK/ALLOW DELETE ONLY IF USER DELETING OWN POST? (OR ADMIN)
     return $this->DB->query("DELETE FROM `comments` WHERE `comment_id`=?", [$cid]);
   }
 
