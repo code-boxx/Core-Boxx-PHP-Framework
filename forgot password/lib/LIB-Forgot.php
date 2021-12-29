8a1ae4f60978f54f21e2595ff1bafc6786e90240
Code Boxx <code-boxx@mail.com>
2021-12-29 20:48:17 +0800

    29 Dec 2021
    Forgot Password module update.

--- forgot password/lib/LIB-Forgot.php
+++ forgot password/lib/LIB-Forgot.php
@@ -1,8 +1,8 @@
 <?php
 class Forgot extends Core {
   // (A) SETTINGS
-  // Request will be valid for N seconds.
-  // Also to prevent spam, cannot make another request until expire.
+  // request will be valid for n seconds.
+  // also to prevent spam, cannot make another request until expire.
   private $valid = 900; // 15 mins = 900 secs
 
   // (B) GET PASSWORD RESET REQUEST
@@ -14,20 +14,25 @@ class Forgot extends Core {
 
   // (C) PASSWORD RESET REQUEST
   function request ($email) {
-    // (C1) CHECK IF VALID USER
+    // (C1) ALREADY SIGNED IN
+    global $_SESS;
+    if (isset($_SESS["user"])) {
+      $this->error = "You are already signed in.";
+      return false;
+    }
+
+    // (C2) CHECK IF VALID USER
     $this->core->load("Users");
     $user = $this->core->Users->get($email);
-    if ($user===false) { return false; }
     if (!is_array($user)) {
       $this->error = "$email is not registered.";
       return false;
     }
 
-    // (C2) CHECK PREVIOUS REQUEST (PREVENT SPAM)
-    $req = $this->get($user['user_id']);
-    if ($req===false) { return false; }
+    // (C3) CHECK PREVIOUS REQUEST (PREVENT SPAM)
+    $req = $this->get($user["user_id"]);
     if (is_array($req)) {
-      $expire = strtotime($req['reset_time']) + $this->valid;
+      $expire = strtotime($req["reset_time"]) + $this->valid;
       $now = strtotime("now");
       $left = $now - $expire;
       if ($left <0) {
@@ -36,77 +41,91 @@ class Forgot extends Core {
       }
     }
 
-    // (C3) CHECKS OK - CREATE NEW RESET REQUEST
+    // (C4) CHECKS OK - CREATE NEW RESET REQUEST
     $now = strtotime("now");
-    $hash = md5($user['user_email'] . $now); // Random hash
-    if (!$this->DB->insert("password_reset",
+    $hash = md5($user["user_email"] . $now); // random hash
+    $this->DB->insert("password_reset",
       ["user_id", "reset_hash", "reset_time"],
-      [$user['user_id'], $hash, date("Y-m-d H:i:s")], true
-    )) { return false; }
+      [$user["user_id"], $hash, date("Y-m-d H:i:s")], true
+    );
 
-    // (C4) SEND EMAIL TO USER
+    // (C5) SEND EMAIL TO USER
     $this->core->load("Mail");
     return $this->core->Mail->send([
-      // @TODO - SET YOUR OWN EMAIL MESSAGE + RESET LINK
-      "to" => $user['user_email'],
+      // @TODO - SET YOUR OWN EMAIL
+      "from" => "sys@site.com",
+      "to" => $user["user_email"],
       "subject" => "Password Reset",
-      "body" => "<a href='".HOST_BASE."2-forgot-reset.php?i={$user['user_id']}&h={$hash}'>RESET</a>"
+      "template" => PATH_PAGES . "MAIL-forgot-a.php",
+      "vars" => [
+        "link" => HOST_BASE."forgot?i={$user["user_id"]}&h={$hash}"
+      ]
     ]);
   }
 
   // (D) PROCESS PASSWORD RESET
   function reset ($id, $hash) {
-    // (D1) CHECKS
-    // GET REQUEST
+    // (D1) ALREADY SIGNED IN
+    global $_SESS;
+    if (isset($_SESS["user"])) {
+      $this->error = "You are already signed in.";
+      return false;
+    }
+
+    // (D2) CHECK REQUEST
     $req = $this->get($id);
-    if ($req===false) { return false; }
     $pass = is_array($req);
-    // CHECK EXPIRE
+
+    // (D3) CHECK EXPIRE
     if ($pass) {
-      $expire = strtotime($req['reset_time']) + $this->valid;
+      $expire = strtotime($req["reset_time"]) + $this->valid;
       $now = strtotime("now");
       $pass = $now <= $expire;
     }
-    // CHECK HASH
-    if ($pass) { $pass = $hash==$req['reset_hash']; }
-    // GET USER
+
+    // (D4) CHECK HASH
+    if ($pass) { $pass = $hash==$req["reset_hash"]; }
+
+    // (D5) GET USER
     if ($pass) {
       $this->core->load("Users");
       $user = $this->core->Users->get($id);
-      if ($user===false) { return false; }
       $pass = is_array($user);
     }
-    // CHECK FAIL - INVALID REQUEST
+
+    // (D6) CHECK FAIL - INVALID REQUEST
     if (!$pass) {
       $this->error = "Invalid request.";
       return false;
     }
 
-    // (D2) CHECK PASS - PROCEED RESET
-    // RANDOM NEW PASSWORD
-    $password = $this->core->random(12);
-    // UPDATE USER PASSWORD
+    // (D7) CHECK PASS - PROCEED RESET
+    // (D7-1) UPDATE USER PASSWORD
     $this->DB->start();
-    $pass = $this->DB->update(
+    $password = $this->core->random(5);
+    $this->DB->update(
       "users", ["user_password"], "`user_id`=?",
       [password_hash($password, PASSWORD_DEFAULT), $id]
     );
-    // REMOVE REQUEST
-    if ($pass) {
-      $pass = $this->DB->query("DELETE FROM `password_reset` WHERE `user_id`=?", [$id]);
-    }
-    // EMAIL TO USER
-    if ($pass) {
-      $this->core->load("Mail");
-      $pass = $this->core->Mail->send([
-        // @TODO - SET YOUR OWN EMAIL MESSAGE
-        "to" => $user['user_email'],
-        "subject" => "Password Reset",
-        "body" => "$password"
-      ]);
-    }
-    // CLOSE
+
+    // (D7-2) REMOVE REQUEST
+    $this->DB->delete("password_reset", "`user_id`=?", [$id]);
+
+    // (D7-3) EMAIL TO USER
+    $this->core->load("Mail");
+    $pass = $this->core->Mail->send([
+      // @TODO - SET YOUR OWN EMAIL
+      "from" => "sys@site.com",
+      "to" => $user["user_email"],
+      "subject" => "Password Reset",
+      "template" => PATH_PAGES . "MAIL-forgot-b.php",
+      "vars" => [
+        "password" => $password
+      ]
+    ]);
+
+    // (D8) CLOSE
     $this->DB->end($pass);
-    return $pass;
+    return true;
   }
 }
---
+++ forgot password/pages/PAGE-forgot.php
@@ -0,0 +1,37 @@
+<?php require PATH_PAGES . "TEMPLATE-top.php";
+
+// (A) PART 1 - ENTER EMAIL
+if (!isset($_GET["i"]) && !isset($_GET["h"])) { ?>
+<!-- (A1) JS -->
+<script>
+function forgot () {
+  // FORM DATA
+  let data = new FormData(document.getElementById("forgotform"));
+
+  // API REQUEST
+  fetch("<?=HOST_API?>forgot/request", { method:"post", body:data })
+  .then(res => res.json()).then((res) => {
+    if (res.status) { alert("Click on the link in your email."); }
+    else { alert(res.message); }
+  });
+  return false;
+}
+</script>
+
+<!-- (A2) REQEST FORM -->
+<form onsubmit="return forgot()" id="forgotform">
+  <input type="email" name="email" required/>
+  <input type="submit" value="Reset Request"/>
+</form>
+<?php }
+
+// (B) PART 2 - VALIDATION
+else {
+$_CORE->load("Forgot");
+$pass = $_CORE->Forgot->reset($_GET["i"], $_GET["h"]); ?>
+<div><?php
+  if ($pass) { echo "OK - New password sent to your email."; }
+  else { echo $_CORE->error; }
+?></div>
+<?php }
+require PATH_PAGES . "TEMPLATE-bottom.php"; ?>
