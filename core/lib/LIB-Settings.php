d61bc2d522beec168e53491d6dd43eb5bc7813cb
Code Boxx <code-boxx@mail.com>
2022-08-31 21:48:30 +0800

    Update LIB-Settings.php

--- core/lib/LIB-Settings.php
+++ core/lib/LIB-Settings.php
@@ -3,19 +3,47 @@ class Settings extends Core {
   // (A) CONSTRUCTOR - LOAD SYSTEM SETTINGS
   function __construct ($core) {
     parent::__construct($core);
+    $this->defineG(1);
+  }
+
+  // (B) AUTO DEFINE BY SETTING GROUP
+  //  $group : setting group
+  function defineG ($group) {
     foreach ($this->DB->fetchKV(
       "SELECT * FROM `settings` WHERE `setting_group`=?",
-      [1], "setting_name", "setting_value"
+      [$group], "setting_name", "setting_value"
     ) as $k=>$v) { define($k, $v); }
   }
 
-  // (B) GET SETTINGS
+  // (C) AUTO DEFINE BY SETTING NAME
+  //  $name : setting name (string or array)
+  //  $json : json decode setting value?
+  function defineN ($name, $json=false) {
+    // (C1) SQL & DATA
+    $sql = "SELECT * FROM `settings` WHERE `setting_name`";
+    if (is_array($name)) {
+      $sql .= " IN (";
+      foreach ($name as $n) { $sql .= "?,"; }
+      $sql = substr($sql,0,-1) . ")";
+      $data = $name;
+    } else {
+      $sql .= "=?";
+      $data = [$name];
+    }
+
+    // (C2) GET & DEFINE
+    foreach ($this->DB->fetchKV(
+      $sql, $data, "setting_name", "setting_value"
+    ) as $k=>$v) { define($k, ($json?json_decode($v,true):$v)); }
+  }
+
+  // (D) GET SETTINGS
   //  $group : setting group
   function getAll ($group=1) {
     return $this->DB->fetchAll("SELECT * FROM `settings` WHERE `setting_group`=?", [$group]);
   }
 
-  // (C) SAVE SETTINGS
+  // (E) SAVE SETTINGS
   //  $settings : array, key => value
   function save ($settings) {
     foreach ($settings as $k=>$v) {
