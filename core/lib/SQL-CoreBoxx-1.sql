065eef8c1e4432fc16112e53edeb0eb9271425ee
Code Boxx <code-boxx@mail.com>
2022-10-31 19:21:26 +0800

    Update SQL-CoreBoxx-1.sql

--- core/lib/SQL-CoreBoxx-1.sql
+++ core/lib/SQL-CoreBoxx-1.sql
@@ -8,7 +8,11 @@ CREATE TABLE `settings` (
 INSERT INTO `settings` (`setting_name`, `setting_description`, `setting_value`, `setting_group`) VALUES
 ('APP_VER', 'App version', '1', 0),
 ('EMAIL_FROM', 'System email from', 'sys@site.com', 1),
-('PAGE_PER', 'Number of entries per page', '20', 1);
+('PAGE_PER', 'Number of entries per page', '20', 1),
+('D_LONG', 'MYSQL date format (long)', '%e %M %Y', 1),
+('D_SHORT', 'MYSQL date format (short)', '%Y-%m-%d', 1),
+('DT_LONG', 'MYSQL date time format (long)', '%e %M %Y %l:%i:%S %p', 1),
+('DT_SHORT', 'MYSQL date time format (short)', '%Y-%m-%d %H:%i:%S', 1);
 
 ALTER TABLE `settings`
   ADD PRIMARY KEY (`setting_name`),
