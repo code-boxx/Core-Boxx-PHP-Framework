2f336c0d99228978a77c72fa4611f0e3182c731d
Code Boxx <code-boxx@mail.com>
2023-04-24 22:43:01 +0800

    Update API-contents.php

--- dynamic content/lib/API-contents.php
+++ dynamic content/lib/API-contents.php
@@ -1,29 +1,11 @@
 <?php
-// @TODO - PROTECT THE FUNCTIONS
-// E.G. CHECK IF USER IS AN ADMIN
-switch ($_REQ) {
-  // (A) INVALID REQUEST
-  default:
-    $_CORE->respond(0, "Invalid request", null, null, 400);
-    break;
+// (A) API ENDPOINTS
+$_CORE->autoAPI([
+  "get" => ["Contents", "get"],
+  "getAll" => ["Contents", "getAll"],
+  "save" => ["Contents", "save"],
+  "del" => ["Contents", "del"]
+]);
 
-  // (B) SAVE CONTENT
-  case "save":
-    $_CORE->autoAPI("Contents", "save");
-    break;
-
-  // (C) DELETE CONTENT
-  case "del":
-    $_CORE->autoAPI("Contents", "del");
-    break;
-
-  // (D) GET CONTENT
-  case "get":
-    $_CORE->autoGETAPI("Contents", "get");
-    break;
-
-  // (E) GET ALL OR SEARCH CONTENTS
-  case "getAll":
-    $_CORE->autoGETAPI("Contents", "getAll");
-    break;
-}
+// (B) INVALID REQUEST
+$_CORE->respond(0, "Invalid request", null, null, 400);
