b3bf87cf16492ff59cc05351e1d26769d3739f8e
Code Boxx <code-boxx@mail.com>
2023-08-02 11:44:30 +0800

    Update HOOK-Routes.php

--- core/lib/HOOK-Routes.php
+++ core/lib/HOOK-Routes.php
@@ -19,14 +19,13 @@ $wild = [
 // (C) MANUAL PATH OVERRIDE
 $override = function ($path) {
   // EXAMPLE - REDIRECT TO LOGIN PAGE IF NOT SIGNED IN
-  global $_CORE;
   if (!isset($_SESSION["user"]) && $path!="login/") {
     header("Location: " . HOST_BASE . "login/");
     exit();
   }
 
   // EXAMPLE - TWEAK PATH BASED ON USER ROLE
-  if ($path=="products/" && $_SESSION["user"]["role"]=="admin") {
+  if ($path=="products/" && $_SESSION["user"]["user_level"]=="A") {
     $path = "admin/products/";
   }
 
