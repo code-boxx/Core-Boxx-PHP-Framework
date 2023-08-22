2acad312aeeb3364c930ab5ab821faf38f9efe1f
Code Boxx <code-boxx@mail.com>
2023-08-22 13:13:49 +0800

    Update LIB-OTP.php

--- one time password/lib/LIB-OTP.php
+++ one time password/lib/LIB-OTP.php
@@ -9,7 +9,7 @@ class OTP extends Core {
   function generate ($email) {
     // (B1) GET USER + EXISTING OTP
     $this->Core->load("Users");
-    $user = $this->Users->get($email, "O");
+    $user = $this->Users->get($email, "OTP");
     if (!is_array($user)) {
       $this->error = "$email is not registered";
       return false;
@@ -18,7 +18,7 @@ class OTP extends Core {
     // (B2) CHECK IF THERE IS AN EXISTING OTP REQUESTS
     if (isset($user["hash_code"])) {
       // (B2-1) MUST WAIT FOR EXPIRY BEFORE RESEND
-      $expire = strtotime($user["hash_time"]) + $this->valid;
+      $expire = strtotime($user["hash_time"]) + ($this->valid * 60);
       $now = strtotime("now");
       $left = $now - $expire;
       if ($left < 0) {
@@ -35,13 +35,7 @@ class OTP extends Core {
 
     // (B3) GENERATE RANDOM PASSWORD
     $pass = $this->Core->random($this->passlength);
-    $this->DB->replace("users_hash",
-      ["user_id", "hash_for", "hash_code", "hash_time", "hash_tries"],
-      [
-        $user["user_id"], "O", password_hash($pass, PASSWORD_DEFAULT),
-        date("Y-m-d H:i:s"), 0
-      ]
-    );
+    $this->Users->hashAdd($user["user_id"], "OTP", password_hash($pass, PASSWORD_DEFAULT));
 
     // (B4) SEND OTP VIA EMAIL
     $this->Core->load("Mail");
@@ -52,7 +46,7 @@ class OTP extends Core {
       "template" => PATH_PAGES . "MAIL-otp.php",
       "vars" => [
         "password" => $pass,
-        "link" => HOST_BASE . "otp/2"
+        "link" => HOST_BASE . "otp/2" // @TODO - SET YOUR OWN LINK
       ]
     ]);
   }
@@ -61,7 +55,7 @@ class OTP extends Core {
   function challenge ($email, $pass) {
     // (C1) GET THE OTP ENTRY
     $this->Core->load("Users");
-    $user = $this->Users->get($email, "O");
+    $user = $this->Users->get($email, "OTP");
     if (!is_array($user)) {
       $this->error = "The specified OTP request is not found.";
       return false;
@@ -86,7 +80,7 @@ class OTP extends Core {
       $this->DB->update(
         "users_hash", ["hash_tries"],
         "`user_id`=? AND `hash_for`=?",
-        [$strikes, $user["user_id"], "O"]
+        [$strikes, $user["user_id"], "OTP"]
       );
       if ($strikes >= $this->tries) {
         // @TODO - what happens if otp fails on too many strikes?
@@ -97,10 +91,7 @@ class OTP extends Core {
     }
 
     // (C5) ALL OK - DELETE OTP
-    $this->DB->delete("users_hash",
-      "`user_id`=? AND `hash_for`=?",
-      [$user["user_id"], "O"]
-    );
+    $this->Users->hashDel($user["user_id"], "OTP");
     return true;
   }
 }
