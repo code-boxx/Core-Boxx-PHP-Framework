47bfdb4a21e4e5c8bee1b7420940e468ed2d4b5d
Code Boxx <code-boxx@mail.com>
2023-08-22 21:59:11 +0800

    22 Aug 2023 Passwordless Login
    Added Login with Web Authentication.

---
+++ login with webauthn/README.md
@@ -0,0 +1,40 @@
+## CORE BOXX LOGIN WITH WEBAUTHN MODULE
+https://code-boxx.com/core-boxx-webauthn-login-module/
+
+## REQUIREMENTS
+* [Core Boxx](https://github.com/code-boxx/Core-Boxx/tree/main/core)
+* [Users Module](https://github.com/code-boxx/Core-Boxx/tree/main/users)
+
+## INSTALL
+* Copy/unzip this module into your existing Core Boxx project folder.
+* Run `install-WAIN.php`, this will automatically:
+  - Modify `pages/TEMPLATE-top`, add "Passwordless Login" to the user menu.
+  - Modify `pages/PAGE-login.php`, load additional JS and add "Passwordless Login" button.
+  - Delete `install-WAIN.php` itself.
+
+## NOTES
+* User needs to be registered first.
+* Open the user menu, "Passwordless Login".
+* Simply register for passwordless login - Can be biometric, pin, face recognition, passkey, etc...
+* That's all, the user can now login using the selected passwordless login method.
+
+## LICENSE
+Copyright by Code Boxx
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ login with webauthn/install-WAIN.php
@@ -0,0 +1,86 @@
+<?php
+// (A) START CORE ENGINE
+require __DIR__ . DIRECTORY_SEPARATOR . "lib" . DIRECTORY_SEPARATOR . "CORE-Go.php";
+
+// (B) CHECK - USER LEVEL
+if (!defined("USR_LVL")) {
+  exit("Please install the users module first.");
+}
+
+// (C) ADD "PASSWORLESS LOGIN" TO MENU
+try {
+  // (C1) BACKUP TEMPLATE TOP
+  copy(PATH_PAGES . "TEMPLATE-top.php", PATH_PAGES . "TEMPLATE-top.old");
+
+  // (C2) ADD "PASSWORLESS LOGIN"
+  $data = file(PATH_PAGES . "TEMPLATE-top.php");
+  foreach ($data as $j=>$line) {
+    if (strpos($line, "myaccount") !== false) {
+      array_splice($data, $j+3, 0, [
+        "            ",
+        "<!-- (LOGIN WITH WEBAUTHN) ADDED BY INSTALLER -->\r\n",
+        "            ",
+        '<li><a class="dropdown-item" href="<?=HOST_BASE?>passwordless">' . "\r\n",
+        "              ",
+        '<i class="ico-sm icon-key"></i> Passwordless Login' . "\r\n",
+        "            ",
+        "</a></li>\r\n"
+      ]);
+      break;
+    }
+  }
+
+  // (C3) SAVE TEMPLATE TOP
+  file_put_contents(PATH_PAGES . "TEMPLATE-top.php", implode("", $data));
+  unset($data);
+} catch (Exception $ex) {
+  exit("Unable to update TEMPLATE-top.php - " . $ex->getMessage());
+}
+
+// (D) MODIFY LOGIN PAGE
+try {
+  // (D1) BACKUP LOGIN PAGE
+  copy(PATH_PAGES . "PAGE-login.php", PATH_PAGES . "PAGE-login.old");
+
+  // (D2) ADDITIONAL JS
+  $data = file(PATH_PAGES . "PAGE-login.php");
+  foreach ($data as $j=>$line) {
+    if (strpos($line, "\$_PMETA") !== false) {
+      array_splice($data, $j+1, 0, [
+        "  // (LOGIN WITH WEBAUTHN) ADDED BY INSTALLER\r\n",
+        '  ["s", HOST_ASSETS."PAGE-wa-helper.js", "defer"],' . "\r\n",
+        '  ["s", HOST_ASSETS."PAGE-login-wa.js", "defer"],' . "\r\n"
+      ]);
+      break;
+    }
+  }
+
+  // (D3) PASSWORDLESS LOGIN BUTTON
+  foreach ($data as $j=>$line) {
+    if (strpos($line, "(C2-2) MORE LOGIN") !== false) {
+      array_splice($data, $j+1, 0, [
+        "      <!-- (LOGIN WITH WEBAUTHN) ADDED BY INSTALLER -->\r\n",
+        '      <button type="button" id="wa-in" onclick="wa.go()" disabled class="my-1 btn btn-primary d-flex-inline align-items-center justify-content-center">' . "\r\n",
+        '        <i class="ico-sm icon-key me-1"></i> Passwordless' . "\r\n",
+        "      </button>\r\n"
+      ]);
+      break;
+    }
+  }
+
+  // (D4) SAVE LOGIN PAGE
+  file_put_contents(PATH_PAGES . "PAGE-login.php", implode("", $data));
+  unset($data);
+} catch (Exception $ex) {
+  exit("Unable to update PAGE-login.php - " . $ex->getMessage());
+}
+
+// (E) DELETE THIS SCRIPT
+try {
+  unlink(PATH_BASE . "install-WAIN.php");
+} catch (Exception $ex) {
+  exit("Unable to delete install-WAIN.php, please do so manually.");
+}
+
+// (F) DONE
+echo "WebAuthn Login module successfully installed.";
