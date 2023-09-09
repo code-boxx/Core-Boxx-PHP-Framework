1d619540d590439dde5aa5aa73ee30920ece047b
Code Boxx <code-boxx@mail.com>
2023-09-09 22:15:13 +0800

    Update 0-setup.bat

--- ai chatbot/chatbot/0-setup.bat
+++ ai chatbot/chatbot/0-setup.bat
@@ -7,13 +7,12 @@ if "%1"=="CPU" (
   pip install torch torchvision torchaudio --force-reinstall
   set FORCE_CMAKE=1
   set CMAKE_ARGS=-DLLAMA_CUBLAS=OFF
-  pip install llama-cpp-python
 ) else (
   pip install torch torchvision torchaudio --force-reinstall --index-url https://download.pytorch.org/whl/cu117
   set FORCE_CMAKE=1
   set CMAKE_ARGS=-DLLAMA_CUBLAS=ON
-  pip install --no-cache-dir --upgrade --force-reinstall llama-cpp-python
 )
+pip install --no-cache-dir --upgrade --force-reinstall llama-cpp-python
 python b_create.py
 if "%1"=="CPU" (
   echo "Install complete - Please download your own model before running 2-bot.bat"
