48cac150283408d6d7a98788026f8ad0b79b61f5
Code Boxx <code-boxx@mail.com>
2023-09-12 20:32:20 +0800

    12 Sep 2023
    Added user ID to push table - Possible to send push to specific users only.

--- web push/lib/SQL-WebPush.sql
+++ web push/lib/SQL-WebPush.sql
@@ -1,7 +1,9 @@
 CREATE TABLE `webpush` (
   `endpoint` varchar(255) NOT NULL,
+  `user_id` bigint(20) NULL,
   `data` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 ALTER TABLE `webpush`
-  ADD PRIMARY KEY (`endpoint`);
+  ADD PRIMARY KEY (`endpoint`),
+  ADD KEY `user_id` (`user_id`);
